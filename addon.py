14fd957a6434a59139f4e7febda3994427f0b9e5
Testato <Testato@users.noreply.github.com>
2019-05-21 19:29:00 +0200

    LOGDEBUG instead of LOGNOTICE

--- addon.py
+++ addon.py
@@ -8,15 +8,15 @@ from contextlib import contextmanager
 def enabled_addon(addon):
     data = json.loads(xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Addons.GetAddonDetails","params":{"addonid":"'+addon+'","properties":["enabled","installed"]},"id":5}'))
     if "result" in data:
-        xbmc.log('Add-on InputStream Adaptive installed',xbmc.LOGNOTICE)
+        xbmc.log('Add-on InputStream Adaptive installed',xbmc.LOGDEBUG)
         if data["result"]["addon"]["enabled"]:
-            xbmc.log('Add-on InputStream Adaptive enabled',xbmc.LOGNOTICE)
+            xbmc.log('Add-on InputStream Adaptive enabled',xbmc.LOGDEBUG)
         else:
-            xbmc.log('Add-on InputStream Adaptive enabling',xbmc.LOGNOTICE)
+            xbmc.log('Add-on InputStream Adaptive enabling',xbmc.LOGDEBUG)
             result_enabled = xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Addons.SetAddonEnabled","params":{"addonid":"'+addon+'","enabled":true},"id":9}')
-            xbmc.log('Add-on InputStream Adaptive enabled',xbmc.LOGNOTICE)
+            xbmc.log('Add-on InputStream Adaptive enabled',xbmc.LOGDEBUG)
     else:
-        xbmc.log('Add-on InputStream Adaptive not installed',xbmc.LOGNOTICE)
+        xbmc.log('Add-on InputStream Adaptive not installed',xbmc.LOGDEBUG)
     yield
 
 
